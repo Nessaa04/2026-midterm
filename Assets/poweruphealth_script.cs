811de882008470cab1a239e67e1598c9acf971fb
Nessaa04 <vanessaallen1218@gmail.com>
2026-03-11 13:46:59 -0400

    My midterm
    A little maze game where you get coins

---
+++ Assets/poweruphealth_script.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poweruphealth_script : MonoBehaviour
+{
+    public void GetBumped()
+    {
+        Destroy(gameObject);
+    }
+}
